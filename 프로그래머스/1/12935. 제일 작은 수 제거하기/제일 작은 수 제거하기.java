1e8f2b70cfde87ebf24c5d9db72e444810a8ff7d
H8bubble <138587585+H8bubble@users.noreply.github.com>
2025-08-17 16:12:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.43 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[]{-1}; 
+        }
+
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        int[] answer = new int[arr.length - 1];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != index) {
+                answer[j++] = arr[i];
+            }
+        }
+
+        return answer;
+    }
+}
